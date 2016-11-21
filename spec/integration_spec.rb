599d5b02517fd34c76242e0ebbd6596b5b253c13
Michael Chinigo <chinigo@gmail.com>
2016-11-20 19:50:34 -0500

    Integration test and corpus of existing benefits strings.

---
+++ spec/integration_spec.rb
@@ -0,0 +1,13 @@
+Citrus.eval( File.read(File.expand_path('../../benefits.citrus', __FILE__)) )
+
+describe 'integration' do
+  subject { Benefits }
+
+  File.open(File.expand_path('../fixtures/reasonable_benefits.txt', __FILE__), 'r') do |f|
+    i = 0
+    f.each_line do |line|
+      it { should parse(line.chomp.strip) }
+      break if (i+=1) == 400
+    end
+  end
+end
